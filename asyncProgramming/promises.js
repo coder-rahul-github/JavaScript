ae62222338fc54cfc7a4d30aaf0b7ddbef5e235e
RAHUL DAS <dasr6163@gmail.com>
2026-07-12 19:37:58 +0530

    promises

---
+++ asyncProgramming/promises.js
@@ -0,0 +1,18 @@
+// promises - resolve , pending , reject
+
+let promise= new Promise(function (resolve,reject){
+    let success = true
+    if (success){
+        resolve("ho gya");
+    }
+    else{
+        reject("fas gya");
+    }
+});
+promise
+    .then(function (result) {
+        console.log(result);
+    })
+    .catch(function(error){
+        console.log(error)
+    });
