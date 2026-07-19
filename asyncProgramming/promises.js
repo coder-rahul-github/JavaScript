3fe89720c803a66d7ad339a1d5d2aa93090713dc
RAHUL DAS <dasr6163@gmail.com>
2026-07-20 00:10:02 +0530

    event timeout

--- asyncProgramming/promises.js
+++ asyncProgramming/promises.js
@@ -1,22 +1,36 @@
 // promises - resolve , pending , reject
 
-let promise= new Promise(function (resolve,reject){
-    let success = true
-    if (success){
-        resolve("ho gya");
-    }
-    else{
-        reject("fas gya");
-    }
-});
-promise
-    .then(function (result) {
-        console.log(result);
-    })
-    .catch(function(error){
-        console.log(error)
-    })
-    .finally(function(last){
-        console.log("bahar aaja");
+// let promise= new Promise(function (resolve,reject){
+//     let success = true
+//     if (success){
+//         resolve("ho gya");
+//     }
+//     else{
+//         reject("fas gya");
+//     }
+// });
+// promise
+//     .then(function (result) {
+//         console.log(result);
+//     })
+//     .catch(function(error){
+//         console.log(error)
+//     })
+//     .finally(function(last){
+//         console.log("bahar aaja");
         
-    });
+//     });
+
+
+
+new promisesOne (function(resolve,reject){
+    setTimeout(function(){
+        console.log(`hello Rahul`);
+        resolve()
+    },1000)
+        
+})
+.then (function(){
+     console.log(`event resolved`);
+            
+});
